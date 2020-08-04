7075b19df5908dcd396c9c3b313e58c9d1621df0
Jonathan Fox <FoxJonathanP@gmail.com>
2020-08-04 14:25:23 -0700

    adding ads

---
+++ src/components/ads/GoogleAd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdSense from 'react-adsense';
+
+const GoogleAd = (props) => {
+  return (
+    <AdSense.Google
+      client='ca-pub-9033962159068688'
+    />
+  );
+}
+
+export default GoogleAd;
